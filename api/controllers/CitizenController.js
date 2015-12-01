8f06c31ea63a25b3cf7ad66eed4f889d27230026
Jean-Michel <jeanmichely@gmail.com>
2015-12-01 16:19:58 +0100

    Envoyer un message pour un citizen OK

--- api/controllers/CitizenController.js
+++ api/controllers/CitizenController.js
@@ -7,6 +7,16 @@
 
 module.exports = {
 	index: function (req, res) {
+		var render = function(req, res) {
+			res.view('citizen/layout', {
+				user: req.session.user,
+				listAlert: req.session.listAlert,
+				listEvent: req.session.listEvent,
+				listScout: req.session.listScout,
+				users: req.session.users
+			});
+		};
+
 		var findTypesAlert = function(i) {
 			TypeAlert.find ({ event: req.session.listEvent[i].id }).then (function (typesAlert) {
 				req.session.listEvent[i]['typesAlert'] = typesAlert;
@@ -17,31 +27,32 @@ module.exports = {
 					Alert.find ({ user: req.session.user.id }).where({isDeleted : false}).then (function (listAlert) { 
 						req.session.listAlert = listAlert;
 						Scout.query ('SELECT * FROM scout INNER JOIN alert WHERE scout.alert=alert.id AND scout.user='+req.session.user.id, function (err, listScout) { 
-							res.view('citizen/layout', {
-								user: req.session.user,
-								listAlert: req.session.listAlert,
-								listEvent: req.session.listEvent,
-								listScout: listScout
-							});
+							req.session.listScout = listScout;
+							render(req, res);
 						});	
 					});	
 				}
 			});
 		};
 
-		Event.find().where({state : {'>': 0}}).then (function (listEvent) { 
-			req.session.listEvent = listEvent;
-			if ( listEvent.length > 0 ) {
-				findTypesAlert(0);
-			} else {
-				Alert.find ({ user: req.session.user.id }).where({isDeleted : false}).then (function (listAlert) { 
-					res.view('citizen/layout', {
-						user: req.session.user,
-						listAlert: listAlert,
-						listEvent: req.session.listEvent
-					});
-				});			
-			}
+		User.find().then (function (users) { 
+			req.session.users = users;
+
+			Event.find().where({state : {'>': 0}}).then (function (listEvent) { 
+				req.session.users = users;
+				req.session.listEvent = listEvent;
+				if ( listEvent.length > 0 ) {
+					findTypesAlert(0);
+				} else {
+					Alert.find ({ user: req.session.user.id }).where({isDeleted : false}).then (function (listAlert) { 
+						res.view('citizen/layout', {
+							user: req.session.user,
+							listAlert: listAlert,
+							listEvent: req.session.listEvent
+						});
+					});			
+				}
+			});
 		});
 	}
 };
